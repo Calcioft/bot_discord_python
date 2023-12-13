1dd40e085e1d6b4ba2c4ed838a43593f9de97167
Cassio <cassioferreiracunhafilho@gmail.com>
2023-12-13 11:14:44 -0300

    adicionando switch liga e desliga e desligando bot python

--- main.py
+++ main.py
@@ -3,25 +3,34 @@ import discord
 from discord.ext import commands
 
 bot_teste = True  # <----------------------- Alterar entre bot teste e bot normal
-
+ligar = True
 
 def run():
     intents = discord.Intents.default()
-
-    discord.Intents.all()
-
-    bot = commands.Bot(command_prefix="!", intents=intents)
+    discord.Intents()
+    bot = commands.Bot(command_prefix="*", intents=intents)
 
     @bot.event
     async def on_ready():
         print(bot.user)
         print(bot.user.id)
         print("______________")
-    @bot.command()
+    @bot.command(
+        aliases=['p'],
+        help="Responde com pong e o ping atual do bot",
+        brief="Responde com pong e o ping atual do bot",
+        eneable=True,
+        hidden=False
+    )
     async def ping(ctx):
         await ctx.send("pong")
 
-    bot.run(config.token)
+    @bot.command()
+    async def helpy(ctx):
+        await ctx.send("ajuda")
+
+    if ligar:
+        bot.run(config.token)
 
 
 if __name__ == "__main__":
